b615a02b871cc633b89d746a9a7ad5c1f125614b
awvolin <alex.volin123@gmail.com>
2023-08-17 18:47:03 -0400

    Initial Commit

---
+++ PhillyCustomDJ/PhillyCustomDJApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhillyCustomDJApp.swift
+//  PhillyCustomDJ
+//
+//  Created by Alex Volin on 8/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PhillyCustomDJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
