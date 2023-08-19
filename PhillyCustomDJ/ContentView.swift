65dc054a416684178b6e53ed6acc6fdce2d1cec1
awvolin <alex.volin123@gmail.com>
2023-08-19 16:28:10 -0400

    test

--- PhillyCustomDJ/ContentView.swift
+++ PhillyCustomDJ/ContentView.swift
@@ -7,26 +7,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selection: Int = 0
-
+    
     var body: some View {
-        TabView(selection: $selection) {
-            CocktailView()
-                .tabItem {
-                    Image(systemName: "music.note")
-                    Text("Mixes")
-                }
-                .tag(1)
-            EventView()
-                .tabItem {
-                    Image(systemName: "person")
-                    Text("My Event")
-                }
+        Button(action: testFont) {
+            Text("test")
         }
+
+    
+        Text("This")
+            .font(Font.custom("TCM.tff", size: 120))
+        //        TabView(selection: $selection) {
+        //            CocktailView()
+        //                .tabItem {
+        //                    Image(systemName: "music.note")
+        //                    Text("Mixes")
+        //                }
+        //                .tag(1)
+        //            EventView()
+        //                .tabItem {
+        //                    Image(systemName: "person")
+        //                    Text("My Event")
+        //                }
+        //        }
+        
+        
     }
 }
 
-
-
+func testFont() {
+    for family: String in UIFont.familyNames {
+        print("\(family)")
+        for names: String in UIFont.fontNames(forFamilyName: family) {
+            print("== \(names)")
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
