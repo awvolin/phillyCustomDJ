f9c28f9734451fea0415049c40a2acdf90b0bfed
awvolin <alex.volin123@gmail.com>
2023-08-19 17:49:36 -0400

    Main functionality - no database

--- PhillyCustomDJ/CocktailView.swift
+++ PhillyCustomDJ/CocktailView.swift
@@ -1,64 +1,64 @@
-// DetailedView.swift
-// PhillyCustomDJ
-
-import SwiftUI
-
-struct CocktailView: View {
-    var items: [Item] = [
-        Item(title: "Out on a limb vol. 6", imageName: "img1"),
-        Item(title: "Morris Arboretum Cocktail Hour", imageName: "img2"),
-        // Add more items here...
-    ]
-    
-    var body: some View {
-        VStack {
-            Spacer()
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding(.horizontal)
-                .edgesIgnoringSafeArea(.all)
-            
-            ScrollView {
-                VStack(alignment: .leading, spacing: 12) {
-                    ForEach(items, id: \.title) { item in
-                        HStack {
-                            Image(item.imageName)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 75, height: 75)
-                                .clipped()
-                            Text(item.title)
-                                .font(Font.custom("TwCenMT-Regular", size: 30))
-                                .colorInvert()
-                            
-                            Spacer()
-                            
-                            
-                        }
-                        .padding([.top, .leading, .trailing])
-                        .cornerRadius(10)
-                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
-                        .padding(0)
-                    }
-                }
-                
-            }
-            
-            
-            
-        }
-        .background(Color(red: 75.0/255, green: 160.0/255, blue: 215.0/255))
-    }
-}
-struct Item {
-    let title: String
-    let imageName: String
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
+//// DetailedView.swift
+//// PhillyCustomDJ
+//
+//import SwiftUI
+//
+//struct CocktailView: View {
+//    var items: [Item] = [
+//        Item(title: "Out on a limb vol. 6", imageName: "img1"),
+//        Item(title: "Morris Arboretum Cocktail Hour", imageName: "img2"),
+//        // Add more items here...
+//    ]
+//
+//    var body: some View {
+//        VStack {
+//            Spacer()
+//            Image("logo")
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .padding(.horizontal)
+//                .edgesIgnoringSafeArea(.all)
+//
+//            ScrollView {
+//                VStack(alignment: .leading, spacing: 12) {
+//                    ForEach(items, id: \.title) { item in
+//                        HStack {
+//                            Image(item.imageName)
+//                                .resizable()
+//                                .aspectRatio(contentMode: .fill)
+//                                .frame(width: 75, height: 75)
+//                                .clipped()
+//                            Text(item.title)
+//                                .font(Font.custom("TwCenMT-Regular", size: 30))
+//                                .colorInvert()
+//
+//                            Spacer()
+//
+//
+//                        }
+//                        .padding([.top, .leading, .trailing])
+//                        .cornerRadius(10)
+//                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+//                        .padding(0)
+//                    }
+//                }
+//
+//            }
+//
+//
+//
+//        }
+//        .background(Color(red: 75.0/255, green: 160.0/255, blue: 215.0/255))
+//    }
+//}
+//struct Item {
+//    let title: String
+//    let imageName: String
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
+//
--- PhillyCustomDJ/ContentView.swift
+++ PhillyCustomDJ/ContentView.swift
@@ -6,39 +6,154 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var selection: Int = 0
+    var mixes: [Item] = [
+        Item(title: "Out On A Limb Vol. 6", imageName: "img1"),
+        Item(title: "Morris Arboretum Cocktail Hour", imageName: "img2"),
+        // Add more items here...
+    ]
+    
+    @State private var selectedItem: Item? = nil
+    
     
     var body: some View {
-        
-                TabView(selection: $selection) {
-                    CocktailView()
-                        .tabItem {
-                            Image(systemName: "music.note")
-                            Text("Mixes")
+        VStack {
+            Spacer()
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
+//            Text("Custom Mixes:")
+//                .padding(.leading)
+//                .frame(maxWidth: .infinity, alignment: .leading)
+//                .font(Font.custom("TwCenMT-Regular", size: 40))
+
+            ScrollView {
+                VStack(alignment: .leading, spacing: 12) {
+                    ForEach(mixes, id: \.title) { item in
+                        HStack {
+                            Image(item.imageName)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 75, height: 75)
+                                .clipped()
+                            Text(item.title)
+                                .font(Font.custom("TwCenMT-Regular", size: 30))
+                                .colorInvert()
+                            
+                            Spacer()
+                            
+                            
                         }
-                        .tag(1)
-                    EventView()
-                        .tabItem {
-                            Image(systemName: "person")
-                            Text("My Event")
+                        .padding([.top, .leading, .trailing])
+                        .cornerRadius(10)
+                        .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                        .onTapGesture {
+                            selectedItem = item
                         }
+                    }
                 }
-        
-        
-    }
-}
-
-func testFont() {
-    for family: String in UIFont.familyNames {
-        print("\(family)")
-        for names: String in UIFont.fontNames(forFamilyName: family) {
-            print("== \(names)")
+                
+            }
         }
+        .background(Color(red: 75.0/255, green: 160.0/255, blue: 215.0/255))
+        .sheet(item: $selectedItem) { item in
+            // Your sample songs
+            let songs = [
+                Song(title: "Controlla", artist: "Drake"),
+                Song(title: "Child's Play", artist: "Drake"),
+                Song(title: "Same Ol' Mistakes", artist: "Rihanna"),
+                Song(title: "Hard to Explain", artist: "The Strokes"),
+                Song(title: "The Kids Don't Stand a Chance", artist: "Vampire Weekend"),
+                Song(title: "Complicated", artist: "Mac Miller"),
+                Song(title: "Every Step", artist: "D.O.D."),
+                Song(title: "Jet Fuel", artist: "Mac Miller"),
+                Song(title: "Controlla", artist: "Drake"),
+                Song(title: "Child's Play", artist: "Drake"),
+                Song(title: "Same Ol' Mistakes", artist: "Rihanna"),
+                Song(title: "Hard to Explain", artist: "The Strokes"),
+                Song(title: "The Kids Don't Stand a Chance", artist: "Vampire Weekend"),
+                Song(title: "Complicated", artist: "Mac Miller"),
+                Song(title: "Every Step", artist: "D.O.D."),
+                Song(title: "Jet Fuel", artist: "Mac Miller"),
+                // Add more songs here...
+            ]
+
+            // Pass the item and songs to the PlaylistView
+            PlaylistView(item: item, songs: songs)
+        }
+
+        .font(Font.custom("TwCenMT-Regular", size: 30))
     }
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
+struct PlaylistView: View {
+    let item: Item
+    let songs: [Song] // An array of songs in the playlist
+
+    var body: some View {
+        VStack {
+            // HStack for the image and title
+            HStack {
+                Image(item.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipped()
+                    .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+                Text(item.title)
+                    .font(Font.custom("TwCenMT-Regular", size: 40))
+                    .shadow(color: Color.black.opacity(0.15), radius: 5, x: 0, y: 5)
+            }
+            
+            .padding()
+
+            // ScrollView for the numbered playlist
+            ScrollView {
+                VStack(alignment: .trailing) {
+                    ForEach(songs.indices, id: \.self) { index in
+                        HStack {
+                            Text("\(index + 1).")
+                                .font(Font.custom("TwCenMT-Regular", size: 24))
+                                .frame(alignment: .leading) // Align the numbers to the leading edge
+
+                            VStack(alignment: .leading) { // Align the song titles and artists to the leading edge
+                                Text(songs[index].title)
+                                    .font(Font.custom("TwCenMT-Regular", size: 24))
+                                Text(songs[index].artist)
+                                    .font(Font.custom("TwCenMT-Regular", size: 20))
+                                    .foregroundColor(.gray)
+                            }
+                            
+                            Spacer() // Fill the remaining space to the right
+                        }
+                        .padding(.horizontal)
+
+                    }
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all) // Ignores safe areas to stretch the background color
+    }
+}
+
+
+
+struct Song {
+    let title: String
+    let artist: String
+}
+
+
+struct Item: Identifiable {
+    let id = UUID()
+    let title: String
+    let imageName: String
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
